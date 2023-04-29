df3fec372c575aa4109c12d9c24f6235aae567cb
HrytsutenkoYaroslav <113903765+HrytsutenkoYaroslav@users.noreply.github.com>
2023-04-29 22:34:00 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import Counter from './Counter.jsx'
 import ClassCounter from './ClassCounter.jsx';
 import "../styles/App.css";
-
+import Post from "./Post.jsx";
 
 
 function App() {
@@ -14,15 +14,7 @@ function App() {
     <div  className="App">
       <ClassCounter/>
       <Counter/>
-      <div className='post'>
-        <div className='post__content'>
-          <strong>javaScript</strong>
-          <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus ratione sed libero, quibusdam hic explicabo nemo optio ullam repellendus velit possimus incidunt vel magnam, recusandae repellat dolorem? Numquam, doloribus adipisci!</div>
-        </div>
-        <div className='post__btns'>
-          <button>delete</button>
-        </div>
-      </div>
+      <Post/>
     </div>
   );
 };
---
+++ src/components/Post.jsx
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+
+
+
+
+const Post = ()  => {
+
+  return(
+    <div className='post'>
+        <div className='post__content'>
+          <strong>javaScript</strong>
+          <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus ratione sed libero, quibusdam hic explicabo nemo optio ullam repellendus velit possimus incidunt vel magnam, recusandae repellat dolorem? Numquam, doloribus adipisci!</div>
+        </div>
+        <div className='post__btns'>
+          <button>delete</button>
+        </div>
+      </div>
+  );
+};
+
+
+export default Post
+
+
+
+
+
+
+
+
