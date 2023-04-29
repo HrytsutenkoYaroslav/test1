065724fff161e04bd2bcdc9f6c5cf6273e3e555c
HrytsutenkoYaroslav <113903765+HrytsutenkoYaroslav@users.noreply.github.com>
2023-04-29 23:41:13 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,19 +2,33 @@ import React, {useState} from 'react';
 import Counter from './Counter.jsx'
 import ClassCounter from './ClassCounter.jsx';
 import "../styles/App.css";
-import Post from "./Post.jsx";
+import PostItem from "./PostItem.jsx";
 
 
 function App() {
 
-  const [value, setValue] = useState('text')
+  const [posts, setPosts] = useState([
+    {id:1, title: 'javaScript', body: 'lorem20'},
+    {id:11, title: 'javaScript', body: 'lorem20'},
+    {id:111, title: 'javaScript', body: 'lorem20'},
+    {id:1111, title: 'javaScript', body: 'lorem20'},
+    {id:11111, title: 'javaScript', body: 'lorem20'},
+    {id:111111, title: 'javaScript', body: 'lorem20'},
+    {id:1111111, title: 'javaScript', body: 'lorem20'},
+    {id:11111111, title: 'javaScript', body: 'lorem20'},
+    {id:111111111, title: 'javaScript', body: 'lorem20'},
+  ])
 
 
   return (
     <div  className="App">
       <ClassCounter/>
       <Counter/>
-      <Post/>
+      {posts.map(post =>
+
+<PostItem post={post}/>
+        )}
+
     </div>
   );
 };
