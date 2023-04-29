2ac11e32e3876d05ac1805f5761ab5df9dce87d0
HrytsutenkoYaroslav <113903765+HrytsutenkoYaroslav@users.noreply.github.com>
2023-04-29 22:10:09 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
-export const App = () => {
+import React, {useState} from 'react';
+import Counter from './Counter.jsx'
+import ClassCounter from './ClassCounter.jsx';
+import "../styles/App.css";
+
+
+
+function App() {
+
+  const [value, setValue] = useState('text')
+
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div  className="App">
+      <ClassCounter/>
+      <Counter/>
+      <div className='post'>
+        <div className='post__content'>
+          <strong>javaScript</strong>
+          <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus ratione sed libero, quibusdam hic explicabo nemo optio ullam repellendus velit possimus incidunt vel magnam, recusandae repellat dolorem? Numquam, doloribus adipisci!</div>
+        </div>
+        <div className='post__btns'>
+          <button>delete</button>
+        </div>
+      </div>
     </div>
   );
 };
+
+
+export default App;
