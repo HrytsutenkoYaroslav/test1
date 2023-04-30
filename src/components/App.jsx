c6e825da1bd6e7db5acf86040b68612c729a5e9c
HrytsutenkoYaroslav <113903765+HrytsutenkoYaroslav@users.noreply.github.com>
2023-04-30 19:55:56 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,9 @@ import Counter from './Counter.jsx'
 import ClassCounter from './ClassCounter.jsx';
 import "../styles/App.css";
 import PostItem from "./PostItem.jsx";
-
-
+import PstList from "./PstList.jsx"
+import MyButton from "./Ui/Button/MyButton.jsx"
+import MyInput from "./Ui/input/MyInput.jsx"
 function App() {
 
   const [posts, setPosts] = useState([
@@ -13,22 +14,30 @@ function App() {
     {id:111, title: 'javaScript', body: 'lorem20'},
     {id:1111, title: 'javaScript', body: 'lorem20'},
     {id:11111, title: 'javaScript', body: 'lorem20'},
-    {id:111111, title: 'javaScript', body: 'lorem20'},
-    {id:1111111, title: 'javaScript', body: 'lorem20'},
-    {id:11111111, title: 'javaScript', body: 'lorem20'},
-    {id:111111111, title: 'javaScript', body: 'lorem20'},
+
   ])
 
+  const [posts2, setPosts2] = useState([
+    {id:1, title: 'javaScript', body: 'lorem20'},
+    {id:11, title: 'javaScript', body: 'lorem20'},
+    {id:111, title: 'javaScript', body: 'lorem20'},
+    {id:1111, title: 'javaScript', body: 'lorem20'},
+    {id:11111, title: 'javaScript', body: 'lorem20'},
+
+  ])
 
   return (
     <div  className="App">
       <ClassCounter/>
       <Counter/>
-      {posts.map(post =>
 
-<PostItem post={post} key={post.id}/>
-        )}
-
+      <form action="">
+        <MyInput type="text" placeholder='title' />
+        <MyInput type="text" placeholder='body' />
+        <MyButton >Create post</MyButton>
+      </form>
+<PstList posts={posts} title="Список постов"/>
+<PstList posts={posts2} title="Список постов 2"/>
     </div>
   );
 };
